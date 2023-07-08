8a8cdec0c1a0d9f2e4a0f268ae2fa127d1ae4d84
Jameson <james.bradlee@telenor.no>
2023-07-09 01:04:14 +0200

    test

--- test.ts
+++ test.ts
@@ -5,4 +5,4 @@ import {
 	bgBlack,
 } from "https://deno.land/std@0.193.0/fmt/colors.ts";
 
-console.log("\u001b[4m\u001b[32mhello world");
+console.log("\u001b[4m\u001b[32m\u001b[58;2;255;255;255mhello world");
