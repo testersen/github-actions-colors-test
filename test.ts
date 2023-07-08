c7bf222433346c8cd2a985f2432a9d6b8ff94045
Jameson <james.bradlee@telenor.no>
2023-07-08 22:27:20 +0200

    test

--- test.ts
+++ test.ts
@@ -5,4 +5,6 @@ import {
 	bgBlack,
 } from "https://deno.land/std@0.193.0/fmt/colors.ts";
 
-console.log(bgBlack(`${red("red")} ${green("green")} ${blue("blue")}`));
+console.log(
+	"\u001b[40;31mRed on black \u001b[32mgreen on black \u001b[34mblue on black",
+);
