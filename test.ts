9be353fa5db547ede58acdb4dc9d2506a3451a03
Jameson <james.bradlee@telenor.no>
2023-07-08 22:32:04 +0200

    test

--- test.ts
+++ test.ts
@@ -6,5 +6,5 @@ import {
 } from "https://deno.land/std@0.193.0/fmt/colors.ts";
 
 console.log(
-	"\u001b[40;31mRed on black \u001b[32mgreen on black \u001b[34mblue on black",
+	"\u001b[45;31mRed on black \u001b[32mgreen on black \u001b[34mblue on black\n",
 );
